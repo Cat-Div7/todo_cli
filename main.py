b0ac21e13d6463f62fd30f98450cbf9387efd533
Cat-Div7 <omarashraf11234@gmail.com>
2026-03-18 22:54:10 +0200

    feat: add basic CLI argument parsing with argparse

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-print("Todo CLI Is Running :)")
+import argparse
+
+parser = argparse.ArgumentParser(description='Todo CLI')
+
+parser.add_argument('command')
+parser.add_argument('task', nargs='?')
+
+args = parser.parse_args()
+
+print(args.command)
+print(args.task)
