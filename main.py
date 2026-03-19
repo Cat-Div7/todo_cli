dfc4f5023823dff6459fefa6e3882ef13ba9e9e4
Cat-Div7 <omarashraf11234@gmail.com>
2026-03-19 03:55:24 +0200

    feat: add list method to display tasks

--- main.py
+++ main.py
@@ -24,7 +24,28 @@ def add_task(task):
   # Write the updated tasks back to the file
   with open('tasks.json', 'w') as f:
     json.dump(curr_tasks, f, indent=2)
+    print('Task Added')
+
+def list_tasks():
+  # Open Folder and read tasks
+  try:
+    with open('tasks.json', 'r') as f:
+      curr_tasks = json.load(f)
+  except (json.JSONDecodeError, FileNotFoundError):
+    curr_tasks = []
+
+  # Return if no tasks
+  if not curr_tasks:
+    print('No tasks yet!')
+    return
+
+  # Display tasks
+  print("Your Tasks:")
+  for idx, task in enumerate(curr_tasks, start=1):
+    status = 'Done' if task['done'] else 'Not Done'
+    print(f"[{idx}] {task['Title']} - {status}")
 
 if command == 'add':
   add_task(task)
-  print('Task Added')
+elif command == 'list':
+  list_tasks()
