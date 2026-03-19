9531c9c15e3c76e0cf907010a647f141c7a5a53c
Cat-Div7 <omarashraf11234@gmail.com>
2026-03-19 03:37:03 +0200

    feat: add 'add task' feature and JSON storage

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import argparse
+import json 
 
 parser = argparse.ArgumentParser(description='Todo CLI')
 
@@ -7,5 +8,25 @@ parser.add_argument('task', nargs='?')
 
 args = parser.parse_args()
 
-print(args.command)
-print(args.task)
+command = args.command
+task = args.task
+
+def add_task(task):
+  # Read existing tasks
+  try:
+    with open('tasks.json', 'r') as f:
+      curr_tasks = json.load(f)
+  except json.JSONDecodeError:
+    curr_tasks = []
+  except FileNotFoundError:
+    curr_tasks = []
+
+  # Append new task
+  curr_tasks.append({"Title": task, "done": False})
+  # Write the updated tasks back to the file
+  with open('tasks.json', 'w') as f:
+    json.dump(curr_tasks, f, indent=2)
+
+if command == 'add':
+  add_task(task)
+  print('Task Added')
